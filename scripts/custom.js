62a3b743c25c55aabe3e326e07159232b7fedc22
jimmywilson11 <jimmy.wilson11@gmail.com>
2019-12-21 11:08:15 +0200

    Try add mouseover video posted. UPD: styles directors.html. ADD: directors second page. Fix script for mouseover

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,8 +1,10 @@
 var figure = $(".video").hover( hoverVideo, hideVideo );
 
 function hoverVideo(e) {  
-    $('video', this).get(0).play(); 
+    $('video', this).get(0).play();
+        $(this).find(".overlay-poster").hide(); 
 }
 function hideVideo(e) {
-    $('video', this).get(0).pause(); 
+    $('video', this).get(0).pause();
+        $(this).find(".overlay-poster").show(); 
 }
